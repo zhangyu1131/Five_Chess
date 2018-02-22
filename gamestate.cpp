ead3311c701cb298e52edb40c8dfcb054463fc7a
Rain <923446353@qq.com>
2018-02-23 00:11:35 +0800

    learn sth about pve, write a general calculate function

--- gamestate.cpp
+++ gamestate.cpp
@@ -26,6 +26,17 @@ void GameState::StartGame()
     gameStatus=PLAYING;
     playerFlag=true;
     qDebug()<<"start game!";
+   if(gameModel==PVE)//如果是PVE模式，则需要初始化评分数组
+   {
+       scoreMapVec.clear();
+       for (int i = 0; i < 15; i++)
+       {
+             std::vector<int> lineScores;
+             for (int j = 0; j < 15; j++)
+                    lineScores.push_back(0);
+             scoreMapVec.push_back(lineScores);
+       }
+   }
 }
 
 //落子后判断是否胜利，即游戏结束
@@ -110,4 +121,63 @@ bool GameState::isWin(int x, int y)
     }
     return false;
 
+}
+
+//计算每个点评分，用于电脑下期
+void GameState::calculateScore()
+{
+    //先全部置0
+    //清空上一局棋盘
+    for (int i = 0; i < 15; i++)
+    {
+        for (int j = 0; j < 15; j++)
+            scoreMapVec[i][j]=0;
+    }
+    maxPoints.clear();
+    int maxScore=0;
+    for(int i=0;i<15;i++)
+    {
+        for(int j=0;j<15;j++)
+        {
+            scoreMapVec[i][j]+=calculateRowScore(i,j);//横
+            scoreMapVec[i][j]+=calculateColScore(i,j);//纵
+            scoreMapVec[i][j]+=calculateMainDiagonalScore(i,j);//主对角线
+            scoreMapVec[i][j]+=calculateViceDiagonalScore(i,j);//副对角线
+            if(scoreMapVec[i][j]>maxScore)
+            {
+                maxPoints.clear();
+                maxPoints.push_back(QPoint(i,j));
+            }
+            else if(scoreMapVec[i][j]==maxScore)
+                maxPoints.push_back(QPoint(i,j));
+        }
+    }
+}
+//计算横向得分
+int GameState::calculateRowScore(int x, int y)
+{
+    int score=0;
+    int i=0;
+    for(i=1;i<=4;i++)
+    {
+        if(x-i<0||gameMapVec[x-i][y]!=-1)
+            break;
+    }
+
+    return score;
+}
+//计算纵向得分
+int GameState::calculateColScore(int x, int y)
+{
+    return 0;
+}
+//计算主对角线方向得分
+int GameState::calculateMainDiagonalScore(int x, int y)
+{
+    return 0;
+}
+//计算副对角线得分
+int GameState::calculateViceDiagonalScore(int x, int y)
+{
+    return 0;
 }
--- gamestate.h
+++ gamestate.h
@@ -3,7 +3,7 @@
 #ifndef GAMESTATE_H
 #define GAMESTATE_H
 #include<vector>
-
+#include<QPoint>
 const int kBoardMargin = 30; // 棋盘边缘空隙
 const int kRadius = 15; // 棋子半径
 const int kMarkSize = 6; // 落子标记边长
@@ -15,6 +15,8 @@ const int kAIDelay = 700; // AI下棋的思考时间
 enum GameModel{PVP,PVPONLINE,PVE};
 //游戏状态
 enum GameStatus{PLAYING,END,CLOSED};
+//pve中的分值数组，从前往后依次是死一，活一，死二，活二，死三……五连。
+std::vector<int> chessScore={4,20,90,400,800,6000,10000,20000,50000};
 
 class GameState
 {
@@ -23,11 +25,19 @@ public:
     GameState();
     void StartGame();
     bool isWin(int x,int y);
+    //计算每个点的评分，电脑根据评分高低落子
+    void calculateScore();
 public:
     std::vector<std::vector<int>> gameMapVec; // 存储当前游戏棋盘和棋子的情况,空白为0，白子1，黑子-1
     std::vector<std::vector<int>> scoreMapVec; // 存储各个点位的评分情况，作为AI下棋依据
     bool playerFlag; // 标示下棋方
     GameModel gameModel; // 游戏模式
     GameStatus gameStatus; // 游戏状态
+    std::vector<QPoint> maxPoints;
+private:
+    int calculateRowScore(int x,int y);
+    int calculateColScore(int x,int y);
+    int calculateMainDiagonalScore(int x,int y);
+    int calculateViceDiagonalScore(int x,int y);
 };
 #endif // GAMESTATE_H
